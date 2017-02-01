fc28c61bc9dc34275c3b17ef93ccdf8a7f0887db
Hassan-Ali-Hassan <eng.hassan.ali.hassan@gmail.com>
2017-02-01 21:18:36 +0200

    Now the code is able to take instructions from the GET request received from another client (a browser for example).

--- ESP_HTTP_server.ino
+++ ESP_HTTP_server.ino
@@ -1,7 +1,9 @@
 #include <ESP8266WiFi.h>
 #include<ESP8266WebServer.h>
+#include <ESP8266mDNS.h> //this is used to assign to the module a distinct web address, rather than use its ip address for making requests
 
-ESP8266WebServer server;
+ESP8266WebServer server(80);
+MDNSResponder mdns;
 char* ssid = "AeroStaff-4";
 char* password = "stewart2";
 void setup() 
@@ -16,11 +18,16 @@ void setup()
   Serial.println();
   Serial.print("IP Address:  ");
   Serial.println(WiFi.localIP());
-
+  if(mdns.begin("Myesp8266"/*instead of using .com as a domain name, the default will be .local*/,WiFi.localIP()))
+  {
+    Serial.print("mdns responder has started");
+  }
   /*adding routes to our webserver*/
   server.on("/test",echo);
   server.on("/test2",echo2);
   server.begin();
+
+  MDNS.addService("http","tcp",80);
 }
 
 void loop() 
@@ -40,6 +47,11 @@ void echo2()
 {
   static int counter = 0;
   counter++;
+  String n = server.arg("p1");
+  String m = server.arg("p2");
+  Serial.print(m.length());
+  Serial.print("\t");
+  Serial.println(n);
   server.send(200,"text/plain","hello man!!"+String(counter)); //this is the response the clients should receive when they send this request
 //  Serial.println("I have received a message from somewhere");
 }
