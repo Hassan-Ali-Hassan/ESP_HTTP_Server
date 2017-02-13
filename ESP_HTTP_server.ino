6e45a94cd0764f5cadf31e085e144138b4e2c07c
Hassan-Ali-Hassan <eng.hassan.ali.hassan@gmail.com>
2017-02-13 10:17:02 +0200

    Now the code has two options, it can send customized packages of information to each agent based on the GET request it receives, and also it can parse the GET request to answer satisfy a custom made inquiry by any agent.

--- ESP_HTTP_server.ino
+++ ESP_HTTP_server.ino
@@ -6,8 +6,12 @@ ESP8266WebServer server(80);
 MDNSResponder mdns;
 char* ssid = "AeroStaff-4";
 char* password = "stewart2";
-int position[6] = {2,84,756,52,4,78}; //the array containing the x and y positions of all rover
+String flag = "0"; //an variable to give order to move or stop
+//int position[6] = {2,84,756,52,4,78}; //the array containing the x and y positions of all rover
+int position[6] = {0,0,120,275,0,0};
 int INDEX = 0; //which is the index of the robot itself, not that of the entry in the array
+float t = 0;
+float oldTime = 0;
 void setup() 
 {
   Serial.begin(115200);
@@ -25,8 +29,11 @@ void setup()
     Serial.print("mdns responder has started");
   }
   /*adding routes to our webserver*/
-  server.on("/test",echo);
-  server.on("/test2",echo2);
+//  server.on("/test",echo);
+//  server.on("/test2",echo2);
+  server.on("/pos1",pos1Fun);
+  server.on("/pos2",pos2Fun);
+  server.on("/pos3",pos3Fun);
   server.on("/position",managePositions);
   server.begin();
 
@@ -36,6 +43,57 @@ void setup()
 void loop() 
 {
   server.handleClient();
+  t = (float)millis()/1000.0;
+  if(t-oldTime > 0.1)
+  {
+    for(int j = 0; j < 6; j++)
+    {
+      Serial.print(position[j]);
+      Serial.print("\t");
+    }
+    Serial.println(t);
+    oldTime = t;
+  }
+}
+
+void pos1Fun()
+{
+  String response = "#"+flag+"$"; 
+  response += String(position[2])+","+String(position[3])+"#"; //reporting position of agent 2
+  INDEX = 1;
+  String UP = server.arg("up");
+  if(UP.length() > 0)
+  {
+    updatePosition(UP);
+  }
+  server.send(200,"text/plain",response); 
+}
+
+void pos2Fun()
+{
+  String response = "#"+flag+"$";
+  INDEX = 2;
+  response += String(position[0])+","+String(position[1])+"#"; //reporting position of agent 1
+  response += String(position[4])+","+String(position[5])+"#"; //reporting posititonof agent 3
+  String UP = server.arg("up");
+  if(UP.length() > 0)
+  {
+    updatePosition(UP);
+  }
+  server.send(200,"text/plain",response); 
+}
+
+void pos3Fun()
+{
+  String response = "#"+flag+"$";
+  INDEX = 3;
+  response += String(position[2])+","+String(position[3])+"#"; //reporting position of agent 2
+  String UP = server.arg("up");
+  if(UP.length() > 0)
+  {
+    updatePosition(UP);
+  }
+  server.send(200,"text/plain",response); 
 }
 
 void echo()
@@ -68,8 +126,18 @@ void managePositions()
   String p2 = server.arg("p2");
   String p3 = server.arg("p3");
   String I = server.arg("index");
+  String status = server.arg("status");
   String UP = server.arg("up"); //stands for update position
 
+  if(status == "1" || status == "0") //if you receive one of these characters, change the flag value
+  {
+    flag = status;
+    response += flag+"$";
+  }
+  else //the slave is inquiring the flag status, just send flag value to it
+  {
+    response += flag+"$";
+  }
   if(p1.length() > 0)
   {
     response += String(position[0])+","+String(position[1])+"#";
@@ -95,7 +163,7 @@ void managePositions()
 
 void updatePosition(String msg)
 {
-  const int bufferSize = 10;
+  const int bufferSize = 20;
   int i = 0;
   int index = 0;
   int L = msg.length();
@@ -116,7 +184,7 @@ void updatePosition(String msg)
       index++;
     }
   }
-  value[index] = container.toInt();
+  value[index] = container.toInt(); //because at the end of the string there is no ','
   position[2*(INDEX-1)] = value[0];
   position[2*(INDEX-1)+1] = value[1];
 }
